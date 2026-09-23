f6dde164c3687556dfcae9b6cdc72cb4027db32f
mehrdad-bt <mehrdad.babatabar@gmail.com>
2026-09-23 16:11:53 +0330

    Add files via upload

---
+++ font_persian_24.h
@@ -0,0 +1,16 @@
+#ifndef VAZIRMATN_24_VERTICAL_FIXED_H
+#define VAZIRMATN_24_VERTICAL_FIXED_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_font_t font_persian_24;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VAZIRMATN_24_VERTICAL_FIXED_H */
